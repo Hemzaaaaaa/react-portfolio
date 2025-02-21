b8d257d4b3446af00be40529f8b3818094d2be34
Hamza Lamkhailif <lamkhailifhamza@gmail.com>
2025-02-21 20:28:03 +0100

    Added Projects

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -1,15 +1,29 @@
 import { Col } from "react-bootstrap";
+import { FaGithub, FaExternalLinkAlt } from "react-icons/fa"; // Import icons
 
-export const ProjectCard = ({ title, description, imgUrl }) => {
+export const ProjectCard = ({
+  title,
+  description,
+  imgUrl,
+  repoLink,
+  liveLink,
+}) => {
   return (
     <Col size={12} sm={6} md={4}>
       <div className="proj-imgbx">
-        <img src={imgUrl} />
+        <img src={imgUrl} alt={title} />
         <div className="proj-txtx">
           <h4>{title}</h4>
           <span>{description}</span>
+          <div className="buttons"></div>
         </div>
       </div>
+      <a href={repoLink} target="_blank" className="custom-btn github">
+        <FaGithub /> GitHub
+      </a>
+      <a href={liveLink} target="_blank" className="custom-btn live">
+        <FaExternalLinkAlt /> Live Demo
+      </a>
     </Col>
-  )
-}
+  );
+};
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -4,41 +4,31 @@ import projImg1 from "../assets/img/project-img1.png";
 import projImg2 from "../assets/img/project-img2.png";
 import projImg3 from "../assets/img/project-img3.png";
 import colorSharp2 from "../assets/img/color-sharp2.png";
-import 'animate.css';
-import TrackVisibility from 'react-on-screen';
+import "animate.css";
+import TrackVisibility from "react-on-screen";
 
 export const Projects = () => {
-
   const projects = [
     {
-      title: "Business Startup",
-      description: "Design & Development",
+      title: "Hotel Landing Page",
+      description: "HTML, CSS & JavaScript",
       imgUrl: projImg1,
+      repoLink: "https://github.com/Hemzaaaaaa/hotel",
+      liveLink: "https://hemzaaaaaa.github.io/hotel/",
     },
     {
-      title: "Business Startup",
-      description: "Design & Development",
+      title: "E-Commerce Platform",
+      description: "React & Node.js",
       imgUrl: projImg2,
+      repoLink: "https://github.com/Hemzaaaaaa/mern-e-commerce-store",
+      liveLink: "https://mern-e-commerce-store-91qw.onrender.com/",
     },
     {
-      title: "Business Startup",
-      description: "Design & Development",
+      title: "Portfolio Website",
+      description: "Personal Portfolio",
       imgUrl: projImg3,
-    },
-    {
-      title: "Business Startup",
-      description: "Design & Development",
-      imgUrl: projImg1,
-    },
-    {
-      title: "Business Startup",
-      description: "Design & Development",
-      imgUrl: projImg2,
-    },
-    {
-      title: "Business Startup",
-      description: "Design & Development",
-      imgUrl: projImg3,
+      repoLink: "https://github.com/yourusername/portfolio",
+      liveLink: "https://your-live-demo.com/portfolio",
     },
   ];
 
@@ -48,56 +38,68 @@ export const Projects = () => {
         <Row>
           <Col size={12}>
             <TrackVisibility>
-              {({ isVisible }) =>
-              <div className={isVisible ? "animate__animated animate__fadeIn": ""}>
-                <h2>Projects</h2>
-                <p>
-                In this section, you'll find a collection of projects I've worked on, ranging from 
-                simple websites to more complex applications. These projects showcase my skills in front-end 
-                and back-end development, using technologies like React, JavaScript, and Node.js. Each project 
-                reflects my dedication to creating functional, well-designed, and responsive web applications.
+              {({ isVisible }) => (
+                <div
+                  className={
+                    isVisible ? "animate__animated animate__fadeIn" : ""
+                  }
+                >
+                  <h2>Projects</h2>
+                  <p>
+                    In this section, you'll find a collection of projects I've
+                    worked on, ranging from simple websites to more complex
+                    applications. These projects showcase my skills in front-end
+                    and back-end development, using technologies like React,
+                    JavaScript, and Node.js.
                   </p>
-                <Tab.Container id="projects-tabs" defaultActiveKey="first">
-                  <Nav variant="pills" className="nav-pills mb-5 justify-content-center align-items-center" id="pills-tab">
-                    <Nav.Item>
-                      <Nav.Link eventKey="first">Tab 1</Nav.Link>
-                    </Nav.Item>
-                    <Nav.Item>
-                      <Nav.Link eventKey="second">Tab 2</Nav.Link>
-                    </Nav.Item>
-                    <Nav.Item>
-                      <Nav.Link eventKey="third">Tab 3</Nav.Link>
-                    </Nav.Item>
-                  </Nav>
-                  <Tab.Content id="slideInUp" className={isVisible ? "animate__animated animate__slideInUp" : ""}>
-                    <Tab.Pane eventKey="first">
-                      <Row>
-                        {
-                          projects.map((project, index) => {
-                            return (
-                              <ProjectCard
-                                key={index}
-                                {...project}
-                                />
-                            )
-                          })
-                        }
-                      </Row>
-                    </Tab.Pane>
-                    <Tab.Pane eventKey="section">
-                      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque quam, quod neque provident velit, rem explicabo excepturi id illo molestiae blanditiis, eligendi dicta officiis asperiores delectus quasi inventore debitis quo.</p>
-                    </Tab.Pane>
-                    <Tab.Pane eventKey="third">
-                      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque quam, quod neque provident velit, rem explicabo excepturi id illo molestiae blanditiis, eligendi dicta officiis asperiores delectus quasi inventore debitis quo.</p>
-                    </Tab.Pane>
-                  </Tab.Content>
-                </Tab.Container>
-              </div>}
+                  <Tab.Container id="projects-tabs" defaultActiveKey="first">
+                    <Nav
+                      variant="pills"
+                      className="nav-pills mb-5 justify-content-center align-items-center"
+                      id="pills-tab"
+                    >
+                      <Nav.Item>
+                        <Nav.Link eventKey="first">Tab 1</Nav.Link>
+                      </Nav.Item>
+                      <Nav.Item>
+                        <Nav.Link eventKey="second">Tab 2</Nav.Link>
+                      </Nav.Item>
+                      <Nav.Item>
+                        <Nav.Link eventKey="third">Tab 3</Nav.Link>
+                      </Nav.Item>
+                    </Nav>
+                    <Tab.Content
+                      id="slideInUp"
+                      className={
+                        isVisible ? "animate__animated animate__slideInUp" : ""
+                      }
+                    >
+                      <Tab.Pane eventKey="first">
+                        <Row>
+                          {projects.map((project, index) => (
+                            <ProjectCard key={index} {...project} />
+                          ))}
+                        </Row>
+                      </Tab.Pane>
+                      <Tab.Pane eventKey="second">
+                        <p>More projects coming soon...</p>
+                      </Tab.Pane>
+                      <Tab.Pane eventKey="third">
+                        <p>Stay tuned for future updates.</p>
+                      </Tab.Pane>
+                    </Tab.Content>
+                  </Tab.Container>
+                </div>
+              )}
             </TrackVisibility>
           </Col>
         </Row>
       </Container>
-      <img className="background-image-right" src={colorSharp2}></img>
+      <img
+        className="background-image-right"
+        src={colorSharp2}
+        alt="Background"
+      />
     </section>
-  )
-}
+  );
+};
